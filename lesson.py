dfd520c6ac4db60481296b99246774c060fdefdc
Yetunde-Iyanu <awoladeyetunde60@gmail.com>
2025-07-27 22:46:14 +0100

    initial commit

---
+++ lesson.py
@@ -0,0 +1,41 @@
+sliceite_name = "Power Learn Project"
+print (site_name)
+site_name = "I love Software Engineering"
+print(site_name)
+
+a,b,c = 5, 7, "Hello World"
+print(a)
+print(b)
+print(c)
+
+num = 50
+Num =  100
+print(num)
+print(Num)
+
+Langauge = ["python", "web", "java"]
+print (Langauge[2])
+
+products = {'Bigbox', 2025, "Greatness", 23}
+print(products)
+
+capital_city = {"Nigeria": "Lagos", "Kenya": "Nairobi"} 
+print(capital_city)
+
+#Arithemetic operation Addition
+a = 2 + 12
+print(a)
+a = 1030 + 450
+print(a)
+#Multiplication
+t = 100 * 10 
+print(t)
+T = 10 * 24 * 50 * 8
+print(T)
+#Subtraction
+W = 250 - 300
+print(W)
+#Division
+W = 100 // 50
+W = 2000 // 9.5
+print(W)
