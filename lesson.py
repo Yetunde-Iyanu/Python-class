ca67eade207cd4bacc37665bac4690a89ac13acc
Yetunde-Iyanu <awoladeyetunde60@gmail.com>
2025-07-31 06:31:12 +0100

    Final commit

--- lesson.py
+++ lesson.py
@@ -1,4 +1,4 @@
-sliceite_name = "Power Learn Project"
+site_name = "Power Learn Project"
 print (site_name)
 site_name = "I love Software Engineering"
 print(site_name)
@@ -39,3 +39,51 @@ print(W)
 W = 100 // 50
 W = 2000 // 9.5
 print(W)
+# List and Dictionary examples
+prime_number = [2, 3, 5]
+print("List 1:", prime_number)
+even_number = [4, 6, 8]
+print("List 2:", even_number)
+# Adding two items up using extend ()
+prime_number.extend(even_number)
+print("List after append:", prime_number)
+#To Change a list
+Langauges = ['python', 'Swift', 'c++']
+Langauges [2] = 'c'
+print(Langauges)
+# Del from List
+Langauges = ['Boy', 'Girl', 'Female']
+del Langauges [2]
+print(Langauges)
+# Iterating through a List
+
+
+# Creating a SET
+Students_id = {120, 200, 150, 310, 400}
+print('students_ID:', Students_id)
+Vowels = {'a', 'i', 'o','u'}
+print('vowels:', Vowels)
+
+# Tupel; Indexing
+Values = (1, 2, 3, 4, 5)
+print(Values[0])
+print(Values[3])
+
+#  Python Dictionary : Adding element to a python dictionary
+
+capital_city = {"Nigeria": "Lagos", "England": "London"}
+print("Initial Dictionary:", capital_city)
+capital_city["Japan"] = "Tokyo"
+print("updated Dictionary:", capital_city)
+# Python Dictionary : Removing element from a python dictionary
+capital_city = {"Nigeria": "Lagos", "England": "London"}
+print(capital_city)
+del capital_city ["England"]
+print("updated Dictionary:", capital_city)
+# Acessing element
+Fruit = {211: "Apple", 20: "Banana", 90: "Pineapple"}
+print(Fruit[211])
+# Iterating through Dictionary
+Fruit = {211: "Apple", 20: "Banana", 90: "Pineapple"}
+for k in Fruit:
+    print(Fruit[k])
